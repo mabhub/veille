845b31d8a9a2e5994cde3c05b2b9cc430a71f3ca
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-12-02 00:03:56 +0100

    Simplify Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,61 +7,29 @@ class Layout extends React.Component {
   render () {
     const { location, title, children } = this.props;
     const rootPath = `${__PATH_PREFIX__}/`;
-    let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h3>
-      );
-    }
     return (
       <div
         style={{
           marginLeft: 'auto',
           marginRight: 'auto',
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          padding: `${rhythm(3 / 2)} ${rhythm(3 / 4)}`,
         }}
       >
-        {header}
+        {
+          (location.pathname === rootPath)
+            ? (
+              <h1 style={{ ...scale(1.5) }}>
+                <Link style={{ color: 'inherit' }} to="/">{title}</Link>
+              </h1>
+            )
+            : (
+              <h3 style={{ marginBottom: rhythm(-1 / 4) }}>
+                <Link to="/">{title}</Link>
+              </h3>
+            )
+        }
         {children}
       </div>
     );
