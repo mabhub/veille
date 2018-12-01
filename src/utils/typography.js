00cef46f380e3738007baf2f331be7631a49de9f
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-12-02 00:03:25 +0100

    Change Typography.js theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,15 @@
 import Typography from 'typography';
-import Wordpress2016 from 'typography-theme-wordpress-2016';
+import Theme from 'typography-theme-moraga';
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
+Theme.overrideThemeStyles = () => ({
+  'strong > a': {
+    fontWeight: 700,
   },
 });
 
-delete Wordpress2016.googleFonts;
 
-const typography = new Typography(Wordpress2016);
+
+const typography = new Typography(Theme);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
