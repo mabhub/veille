76ee101413489f5611fb4b654a71c47ee49609dc
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2020-02-13 10:11:04 +0100

    Awaitify gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,55 +6,44 @@ const getSiblings = (posts, index) => ({
   next:     index === 0 ? null : posts[index - 1].node,
 });
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  return new Promise((resolve, reject) => {
-    const graphQuery = graphql(`
-      {
-        allMarkdownRemark(
-          sort: {
-            fields: [frontmatter___date],
-            order: DESC
-          },
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const {
+    data: {
+      allMarkdownRemark: { edges = [] } = {},
+    },
+  } = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: {
+          fields: [frontmatter___date],
+          order: DESC
+        },
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
             }
           }
         }
       }
-    `);
+    }
+  `);
 
-    resolve(graphQuery.then(({ errors, data: { allMarkdownRemark } }) => {
-      if (errors) {
-        // eslint-disable-next-line no-console
-        console.log(errors);
-        reject(errors);
-      }
-
-      // Create blog posts pages.
-      const { edges } = allMarkdownRemark;
-      edges.forEach(({ node: { fields: { slug } } }, index, posts) =>
-        createPage({
-          path: slug,
-          component: path.resolve('./src/templates/blog-post.js'),
-          context: { slug, ...getSiblings(posts, index) },
-        }));
+  // Create blog posts pages.
+  edges.forEach(({ node: { fields: { slug } } }, index, posts) =>
+    createPage({
+      path: slug,
+      component: path.resolve('./src/templates/blog-post.js'),
+      context: { slug, ...getSiblings(posts, index) },
     }));
-  });
 };
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-
+exports.onCreateNode = ({ node, actions: { createNodeField }, getNode }) => {
   if (node.internal.type === 'MarkdownRemark') {
     createNodeField({
       name: 'slug',
