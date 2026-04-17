769b43a48a012ba06964fba71ab9171ca3685bfa
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2026-04-17 10:38:26 +0200

    Drop redundant English comment in typography module

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,6 @@
 import Typography from 'typography';
 import themeMoraga from 'typography-theme-moraga';
 
-import '@fontsource/source-sans-pro/200.css';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/source-sans-pro/400-italic.css';
 import '@fontsource/source-sans-pro/700.css';
@@ -16,7 +15,6 @@ themeMoraga.googleFonts = [];
 
 const typography = new Typography(themeMoraga);
 
-// Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
   typography.injectStyles();
 }
