9e709c6c0c3b7ce2a107cf6726d18254912b5dde
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2026-04-17 10:39:17 +0200

    Translate 404 page to French

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import Layout from '../components/Layout';
 
 const NotFoundPage = ({ location }) => (
   <Layout location={location}>
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Page introuvable</h1>
+    <p>Cette page n&rsquo;existe pas (ou plus).</p>
   </Layout>
 );
 
@@ -13,7 +13,7 @@ export default NotFoundPage;
 export const Head = () => (
   <>
     <html lang="fr" />
-    <title>404: Not Found</title>
-    <meta name="description" content="Page not found" />
+    <title>404 — Page introuvable</title>
+    <meta name="description" content="Page introuvable" />
   </>
 );
