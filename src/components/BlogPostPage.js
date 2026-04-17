e11e427be3322c9095caa59a64b01140f8b932d2
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2026-04-17 10:38:59 +0200

    Unify React import style across components

--- src/components/BlogPostPage.js
+++ src/components/BlogPostPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { rhythm, scale } from '../utils/typography';
 import Layout from './Layout';
