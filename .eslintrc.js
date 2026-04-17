a5c806361e462070abb5fb6f6171ddd652fbea94
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2026-04-17 10:10:10 +0200

    Remove unused severity constants from ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
-const off   = 'off';
-const warn  = 'warn';
-const error = 'error';
-
 module.exports = {
   extends: 'makina',
   globals: {
-    'graphql': false,
-    '__PATH_PREFIX__': true,
+    graphql: false,
+    __PATH_PREFIX__: true,
   },
 };
